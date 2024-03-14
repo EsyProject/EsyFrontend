72887c6b6c54b0969e1b9e961fd39eb6d3567678
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-03-14 11:28:44 -0300

    style: change navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import "material-symbols";
 import "./Navbar.css";
 
 const Navbar = ({ currentPageIcon }) => {
   return (
     <div className="navbar-container">
-      <div className="navbar-wrapper">
-        <div className="navbar-content">
-          <div className="rectangle">
-            <span className="material-symbols-outlined">{currentPageIcon}</span>
-          </div>
+      <div className="navbar-content">
+        <div className="rectangle">
+          <span className="material-symbols-outlined">{currentPageIcon}</span>
         </div>
+        <h2>Meus Eventos</h2>
+        <h4>Próximos eventos</h4>
+        <h4>Histórico de eventos</h4>
       </div>
     </div>
   );
