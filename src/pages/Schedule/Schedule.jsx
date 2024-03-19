2bf636b41e06973ca928c0ef82087333b1685a68
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-03-19 14:48:36 -0300

    feat: add new page Schedule.jsx

--- src/pages/Schedule/Schedule.jsx
+++ src/pages/Schedule/Schedule.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+import { Sidebar, Navbar } from "../../pages/index";
+
 
 const Schedule = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
-    <div>Schedule</div>
+    <div className={`historic-container ${sidebarOpen ? "sidebar-open" : ""}`}>
+      <Navbar currentPageIcon="calendar_month" activePage="schedule" />
+
+      <Sidebar
+        activePage="history"
+        isOpen={sidebarOpen}
+        toggleSidebar={toggleSidebar}
+      />
+
+      <div className="container-main">
+        <h1>Conteudo aqui</h1>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, fugiat quia magnam iure, doloribus expedita vero porro molestias maiores</span>
+      </div>
+    </div>
   )
 }
 
