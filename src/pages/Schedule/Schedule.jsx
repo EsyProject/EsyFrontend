bf026d0d471c07b98a2364df3f28b6274d41bd90
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-03-19 15:35:35 -0300

    style: change navbar style

--- src/pages/Schedule/Schedule.jsx
+++ src/pages/Schedule/Schedule.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Sidebar, Navbar } from "../../pages/index";
 
-
 const Schedule = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
@@ -14,17 +13,20 @@ const Schedule = () => {
       <Navbar currentPageIcon="calendar_month" activePage="schedule" />
 
       <Sidebar
-        activePage="history"
+        activePage="calendar"
         isOpen={sidebarOpen}
         toggleSidebar={toggleSidebar}
       />
 
       <div className="container-main">
         <h1>Conteudo aqui</h1>
-        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, fugiat quia magnam iure, doloribus expedita vero porro molestias maiores</span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, fugiat
+          quia magnam iure, doloribus expedita vero porro molestias maiores
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Schedule
+export default Schedule;
