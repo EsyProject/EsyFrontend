c8e8b68f8c5cd0cc22f5cff9036130fa0192c885
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-03-20 10:41:00 -0300

    feat: add new file for a list of events

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,23 +1,36 @@
-
+import eventsList from "./data";
 
 const Searchbar = () => {
   return (
     <div>
-      
       <input type="search" />
 
-      <ul>
-        <li>
-          <p>Workshop de Desenvolvimento de Aplicativos Móveis</p>
-          <p>Seg, 27 Mai 2024</p>
-          <p>10:00</p>
-          <p>Ca300</p>
-          <p>BISB</p>
-          <p>SIM</p>
-        </li>
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome do Evento</th>
+            <th>Data</th>
+            <th>Horário</th>
+            <th>Local</th>
+            <th>Área</th>
+            <th>Presença</th>
+          </tr>
+        </thead>
+        <tbody>
+          {eventsList.map((event, index) => (
+            <tr key={index}>
+              <td>{event.name}</td>
+              <td>{event.date}</td>
+              <td>{event.time}</td>
+              <td>{event.local}</td>
+              <td>{event.area}</td>
+              <td>{event.presence}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Searchbar
+export default Searchbar;
---
+++ src/components/Searchbar/data/index.jsx
@@ -0,0 +1,84 @@
+const eventsList = [
+  {
+    name: "Workshop de Desenvolvimento",
+    date: "Seg, 27 Mai 2024",
+    time: "10:00",
+    local: "Ca300",
+    area: "BISB",
+    presence: "SIM"
+  },
+  {
+    name: "Seminário de Inteligência Artificial",
+    date: "Ter, 28 Mai 2024",
+    time: "14:00",
+    local: "Auditório Principal",
+    area: "TI",
+    presence: "NÃO"
+  },
+  {
+    name: "Conferência de Ciência de Dados",
+    date: "Qua, 29 Mai 2024",
+    time: "09:30",
+    local: "Sala 201",
+    area: "Ciência de Dados",
+    presence: "SIM"
+  },
+  {
+    name: "Curso Avançado de React Native",
+    date: "Qui, 30 Mai 2024",
+    time: "13:00",
+    local: "Laboratório de Informática",
+    area: "Desenvolvimento Web",
+    presence: "SIM"
+  },
+  {
+    name: "Palestra sobre Blockchain",
+    date: "Sex, 31 Mai 2024",
+    time: "11:00",
+    local: "Sala de Conferências",
+    area: "Tecnologia Financeira",
+    presence: "SIM"
+  },
+  {
+    name: "Hackathon de Inteligência Artificial",
+    date: "Seg, 03 Jun 2024",
+    time: "08:00",
+    local: "Centro de Inovação",
+    area: "TI",
+    presence: "SIM"
+  },
+  {
+    name: "Workshop de UX Design",
+    date: "Ter, 04 Jun 2024",
+    time: "10:30",
+    local: "Sala de Treinamento",
+    area: "Design",
+    presence: "SIM"
+  },
+  {
+    name: "Congresso de Big date",
+    date: "Qua, 05 Jun 2024",
+    time: "09:00",
+    local: "Salão Nobre",
+    area: "Big date",
+    presence: "SIM"
+  },
+  {
+    name: "Curso de Python para Iniciantes",
+    date: "Qui, 06 Jun 2024",
+    time: "15:00",
+    local: "Sala 103",
+    area: "Programação",
+    presence: "NÃO"
+  },
+  {
+    name: "Conferência de Cibersegurança",
+    date: "Sex, 07 Jun 2024",
+    time: "12:30",
+    local: "Auditório Central",
+    area: "Segurança da Informação",
+    presence: "SIM"
+  }
+];
+
+export default eventsList;
