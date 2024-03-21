f682d4c6ce48525a25974fd2129294a135b9e7bb
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-03-21 09:26:59 -0300

    style: change EventTable.css style

--- src/components/EventTable/EventTable.jsx
+++ src/components/EventTable/EventTable.jsx
@@ -5,38 +5,43 @@ import "./EventTable.css";
 
 const EventTable = ({ events }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th className="th-event-name">Nome do Evento</th>
-          <th className="th-event-date">Data</th>
-          <th className="th-event-time">Horário</th>
-          <th className="th-event-local">Local</th>
-          <th className="th-event-area">Área</th>
-          <th className="th-event-presence">Presença</th>
-        </tr>
-      </thead>
-      <tbody>
-        {events.map((event, index) => (
-          <tr key={index}>
-            <td className="td-event-name">{event.name}</td>
-            <td className="td-event-date">{event.date}</td>
-            <td className="td-event-time">{event.time}</td>
-            <td className="td-event-local">{event.local}</td>
-            <td className="td-event-area">
-              <div className="container-event-area" data-area={event.area}>
-                {event.area}
-              </div>
-            </td>
-            <td className="td-event-presence">
-              <div className="container-event-presence" data-presence={event.presence}>
-                {event.presence}
-              </div>
-            </td>
+    <div className="container-table">
+      <table>
+        <thead>
+          <tr>
+            <th className="th-event-name">Nome do Evento</th>
+            <th className="th-event-date">Data</th>
+            <th className="th-event-time">Horário</th>
+            <th className="th-event-local">Local</th>
+            <th className="th-event-area">Área</th>
+            <th className="th-event-presence">Presença</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {events.map((event, index) => (
+            <tr key={index}>
+              <td className="td-event-name">{event.name}</td>
+              <td className="td-event-date">{event.date}</td>
+              <td className="td-event-time">{event.time}</td>
+              <td className="td-event-local">{event.local}</td>
+              <td className="td-event-area">
+                <div className="container-event-area" data-area={event.area}>
+                  {event.area}
+                </div>
+              </td>
+              <td className="td-event-presence">
+                <div
+                  className="container-event-presence"
+                  data-presence={event.presence}
+                >
+                  {event.presence}
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
