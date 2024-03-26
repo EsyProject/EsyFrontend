eee4983e4d6b5cb3233121208684a7dfdf1abfbb
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-03-26 15:56:22 -0300

    feat: adding link to sidebar for navigation between screens

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { LogoEsy, LogoEsyClosed, IoIosArrowBack } from "../../pages/index";
-import "./Sidebar.css";
 import { SidebarIcon } from "../../pages/index";
+import { Link } from "react-router-dom";
+import "./Sidebar.css";
 
 const Sidebar = ({ isOpen, toggleSidebar, activePage }) => {
   const [activeIcon, setActiveIcon] = useState(null);
@@ -30,68 +31,81 @@ const Sidebar = ({ isOpen, toggleSidebar, activePage }) => {
 
       <nav className="border">
         <div className="sidebar-content">
-          <SidebarIcon
-            iconName="notifications"
-            text="Notificações"
-            buttonClassName="icon-hover"
-            className="notification"
-            textClassName="sidebar-text"
-            active={activeIcon === "notifications"}
-            onClick={() => handleIconClick("notifications")}
-          />
-          <SidebarIcon
-            iconName="home"
-            text="Home"
-            buttonClassName="icon-hover"
-            className="home"
-            textClassName="sidebar-text"
-            active={activeIcon === "home"}
-            onClick={() => handleIconClick("home")}
-          />
+          <Link to="/notifications">
+            <SidebarIcon
+              iconName="notifications"
+              text="Notificações"
+              buttonClassName="icon-hover"
+              className="notification"
+              textClassName="sidebar-text"
+              active={activeIcon === "notifications"}
+              onClick={() => handleIconClick("notifications")}
+            />
+          </Link>
+          <Link to="/home">
+            <SidebarIcon
+              iconName="home"
+              text="Home"
+              buttonClassName="icon-hover"
+              className="home"
+              textClassName="sidebar-text"
+              active={activeIcon === "home"}
+              onClick={() => handleIconClick("home")}
+            />
+          </Link>
           <div className="border-bottom"></div>
           <div className={`nav-text ${isOpen ? "active" : ""}`}>
             <h5>EVENTOS</h5>
           </div>
-          <SidebarIcon
-            iconName="calendar_month"
-            text="Próximos eventos"
-            buttonClassName="icon-hover"
-            className="calendar"
-            textClassName="sidebar-text"
-            active={activeIcon === "calendar"}
-            onClick={() => handleIconClick("calendar")}
-          />
-          <SidebarIcon
-            iconName="history"
-            text="Histórico"
-            buttonClassName="icon-hover"
-            className="clock"
-            textClassName="sidebar-text"
-            active={activeIcon === "history"}
-            onClick={() => setActiveIcon("history")}
-          />
+          <Link to="/schedule">
+            <SidebarIcon
+              iconName="calendar_month"
+              text="Próximos eventos"
+              buttonClassName="icon-hover"
+              className="calendar"
+              textClassName="sidebar-text"
+              active={activeIcon === "calendar"}
+              onClick={() => handleIconClick("calendar")}
+            />
+          </Link>
+          <Link to="/historic">
+            <SidebarIcon
+              iconName="history"
+              text="Histórico"
+              buttonClassName="icon-hover"
+              className="clock"
+              textClassName="sidebar-text"
+              active={activeIcon === "history"}
+              onClick={() => setActiveIcon("history")}
+            />
+          </Link>
           <div className="border-bottom"></div>
           <div className={`nav-text ${isOpen ? "active" : ""}`}>
             <h5>GERAIS</h5>
           </div>
-          <SidebarIcon
-            iconName="person"
-            text="Conta"
-            buttonClassName="icon-hover"
-            className="calendar"
-            textClassName="sidebar-text"
-            active={activeIcon === "account"}
-            onClick={() => handleIconClick("account")}
-          />
-          <SidebarIcon
-            iconName="tune"
-            text="Configurações"
-            buttonClassName="icon-hover"
-            className="settings"
-            textClassName="sidebar-text"
-            active={activeIcon === "settings"}
-            onClick={() => handleIconClick("settings")}
-          />
+
+          <Link to="/account">
+            <SidebarIcon
+              iconName="person"
+              text="Conta"
+              buttonClassName="icon-hover"
+              className="calendar"
+              textClassName="sidebar-text"
+              active={activeIcon === "account"}
+              onClick={() => handleIconClick("account")}
+            />
+          </Link>
+          <Link to="/settings">
+            <SidebarIcon
+              iconName="tune"
+              text="Configurações"
+              buttonClassName="icon-hover"
+              className="settings"
+              textClassName="sidebar-text"
+              active={activeIcon === "settings"}
+              onClick={() => handleIconClick("settings")}
+            />
+          </Link>
         </div>
         <div className="logout-container">
           <SidebarIcon
