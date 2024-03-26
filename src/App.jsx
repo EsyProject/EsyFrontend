044d68ef61fc569e5243ec68f5a66a141ec6ec2b
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-03-26 16:00:25 -0300

    feat: creating routes for existing pages

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
           <Route path='/Login' element={<Login />} />
           <Route path='/Password' element={<Password />} />
           <Route path='/Register' element={<Register />} />
-          <Route path='/Historic' element={<Historic />} />
+          <Route path='/historic' element={<Historic />} />
           <Route path='/schedule' element={<Schedule />} />
         </Routes>
       </Router>
