e160fba108c56f7360a1a51d199f4a4ce7e90824
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-04-01 14:33:06 -0300

    feat: create a new component ButtonLink

---
+++ src/components/ButtonLink/ButtonLink.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const ButtonLink = ({ to, children }) => (
+  <Link to={to} className="custom-button">
+    {children}
+  </Link>
+);
+
+ButtonLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default ButtonLink;
