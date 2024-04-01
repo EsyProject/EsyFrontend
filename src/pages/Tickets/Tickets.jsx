15738751f88a6fd734e08761d53c01a155162038
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-04-01 15:50:35 -0300

    feat: navbar and sidebar settings

--- src/pages/Tickets/Tickets.jsx
+++ src/pages/Tickets/Tickets.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState } from "react";
+/* import PropTypes from "prop-types"; */
+import { Sidebar, Navbar } from "../../pages/index";
+import "./Tickets.css";
 
 const Tickets = () => {
-  return (
-    <div>Tickets</div>
-  )
-}
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-export default Tickets
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  return (
+    <div className={`settings-container ${sidebarOpen ? "sidebar-open" : ""}`}>
+      <Navbar
+        currentPageIcon="confirmation_number"
+        activePage="tickets"
+        navigationText="Meus eventos"
+        showNavigationTexts={true}
+      />
+
+      <Sidebar
+        activePage="confirmation_number"
+        isOpen={sidebarOpen}
+        toggleSidebar={toggleSidebar}
+      />
+
+      <div className="section-container">Tickets</div>
+    </div>
+  );
+};
+
+export default Tickets;
