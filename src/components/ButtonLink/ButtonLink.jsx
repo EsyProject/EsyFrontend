925f646ecefc59444176b68af2b580dafb214d4b
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-04-01 14:54:48 -0300

    feat: adding new file to button css

--- src/components/ButtonLink/ButtonLink.jsx
+++ src/components/ButtonLink/ButtonLink.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import './ButtonLink.css';
 
 const ButtonLink = ({ to, children }) => (
   <Link to={to} className="custom-button">
