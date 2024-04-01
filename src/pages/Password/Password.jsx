8435aa7dca03164bc00766a859837ae2a9893b0a
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-04-01 14:37:03 -0300

    feat: using the component button

--- src/pages/Password/Password.jsx
+++ src/pages/Password/Password.jsx
@@ -1,14 +1,18 @@
-import { useState, useEffect } from 'react';
-import { FiEye, FiEyeOff } from 'react-icons/fi';
-import { ImageRegisterBack, LogoEsy, AnimatedImage } from '../index';
-import { Link } from 'react-router-dom';
-import './Password.css';
+import { useState, useEffect } from "react";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import {
+  ImageRegisterBack,
+  LogoEsy,
+  AnimatedImage,
+  ButtonLink,
+} from "../index";
+import "./Password.css";
 
 const Password = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState("");
   const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
-  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState("");
   const [acceptPolicies, setAcceptPolicies] = useState(false);
   const [acceptEmails, setAcceptEmails] = useState(false);
   const [acceptAll, setAcceptAll] = useState(false);
@@ -50,18 +54,18 @@ const Password = () => {
 
         <div className="container-register">
           <div className="logo-img">
-            <img src={LogoEsy} alt='logo' />
+            <img src={LogoEsy} alt="logo" />
           </div>
 
           <div className="form-container-password">
             <h2>Cadastre-se</h2>
             <form>
-              <label htmlFor='password'>Crie sua senha</label>
+              <label htmlFor="password">Crie sua senha</label>
               <div className="password-input">
                 <input
-                  type={showPassword ? 'text' : 'password'}
-                  id='password'
-                  name='password'
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
@@ -70,38 +74,65 @@ const Password = () => {
                 </button>
               </div>
 
-              <label htmlFor='confirmPassword'>Confirme sua senha</label>
+              <label htmlFor="confirmPassword">Confirme sua senha</label>
               <div className="password-input">
                 <input
-                  type={showPasswordConfirm ? 'text' : 'password'}
-                  id='password'
-                  name='passwordConfirm'
+                  type={showPasswordConfirm ? "text" : "password"}
+                  id="password"
+                  name="passwordConfirm"
                   value={passwordConfirm}
                   onChange={(e) => setPasswordConfirm(e.target.value)}
                 />
                 <button type="button" onClick={handleTogglePasswordConfirm}>
-                  {showPasswordConfirm ? <FiEye size={20} /> : <FiEyeOff size={20} />}
+                  {showPasswordConfirm ? (
+                    <FiEye size={20} />
+                  ) : (
+                    <FiEyeOff size={20} />
+                  )}
                 </button>
               </div>
 
-              <p>Sua senha deve ter no mínimo 8 caracteres e possuir letras, números e caracter especial.</p>
+              <p>
+                Sua senha deve ter no mínimo 8 caracteres e possuir letras,
+                números e caracter especial.
+              </p>
 
               <div className="checkboxes">
                 <label>
-                  <input type="checkbox" checked={acceptPolicies} onChange={handleToggleAcceptPolicies} />
-                  <p>Declaro que li, estou ciente e concorco com os <a href="#"><span>Termos e Políticas de Privacidade e Segurança Esy</span></a>.</p>
+                  <input
+                    type="checkbox"
+                    checked={acceptPolicies}
+                    onChange={handleToggleAcceptPolicies}
+                  />
+                  <p>
+                    Declaro que li, estou ciente e concorco com os{" "}
+                    <a href="#">
+                      <span>
+                        Termos e Políticas de Privacidade e Segurança Esy
+                      </span>
+                    </a>
+                    .
+                  </p>
                 </label>
                 <label>
-                  <input type="checkbox" checked={acceptEmails} onChange={handleToggleAcceptEmails} />
-                  Aceito receber notificações por e-mail sobre os principais eventos que estiverem ocorrendo na Bosch Campinas.
+                  <input
+                    type="checkbox"
+                    checked={acceptEmails}
+                    onChange={handleToggleAcceptEmails}
+                  />
+                  Aceito receber notificações por e-mail sobre os principais
+                  eventos que estiverem ocorrendo na Bosch Campinas.
                 </label>
                 <label>
-                  <input type="checkbox" checked={acceptAll} onChange={handleToggleAcceptAll} />
+                  <input
+                    type="checkbox"
+                    checked={acceptAll}
+                    onChange={handleToggleAcceptAll}
+                  />
                   Aceitar tudo
                 </label>
               </div>
-
-              <Link to="/register" className="custom-button">Cadastrar</Link>
+              <ButtonLink to="/register">Cadastrar</ButtonLink>
             </form>
           </div>
         </div>
