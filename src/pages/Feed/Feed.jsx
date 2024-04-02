010ab1eee27db56f0bda6abdf3cdb0a8cfc5df29
Souza Manuela (CaP/ETS2) <smz9ca@bosch.com>
2024-04-02 16:29:41 -0300

    02/04/2024 - 16:30

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -1,28 +1,107 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from 'react-modal';
 import './Feed.css';
 
 import { FaStar } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
 import { MdOutlineRemoveRedEye } from "react-icons/md";
 
-
-
 const Feed = () => {
+  const dynamicImage = 'src/assets/image-banner.png';
+  // const dynamicImage = 'src/assets/dog.jpg';
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  // flags of modal
+  function toggleActive(button) {
+    button.classList.toggle("active");
+  }
+
+  const buttons = document.querySelectorAll('.flag');
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      toggleActive(button);
+    });
+  });
+
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
+  const handleOptionSelect = (option) => {
+    const isSelected = selectedOptions.includes(option);
+    if (isSelected) {
+      setSelectedOptions(selectedOptions.filter((selectedOption) => selectedOption !== option));
+    } else {
+      setSelectedOptions([...selectedOptions, option]);
+    }
+  };
+
+  // checkbox modal
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  // starts color select
+  const [rating, setRating] = useState(0);
+
+  const handleStarClick = (starIndex) => {
+    setRating(starIndex + 1);
+  };
+
+  // modal for images post
+
+  const [modalOpenImage, setModalOpenImage] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openModalImage = (imageSrc) => {
+    setSelectedImage(imageSrc);
+    setModalOpenImage(true);
+  };
+
+  const closeModalImage = () => {
+    setSelectedImage(null);
+    setModalOpenImage(false);
+  };
+
   return (
     <div className='container-feed'>
+      <style>
+        {`
+          :root {
+            --dynamic-image: url('${dynamicImage}');
+          }
+        `}
+      </style>
+
       <section className="banner">
-        <text>
+        <div className="text">
           <p>ETS - Engineering Technical School</p>
           <h1>Hackathon</h1>
           <h2>Venha conhecer a competição de tecnologia e inovação da ETS</h2>
-          <button>Saber mais</button>
+          <button onClick={openModal}>Saber mais</button>
           <p>7ª Edição - Julho de 2024</p>
-        </text>
-        <div className="container-image-banner">
-          <img src="src\assets\image-banner.png" alt="" className='circle'/>
-          <img src="src\assets\image-banner.png" alt="" className='img'/>
         </div>
+        <div className="container-image-banner">
+          {/* <div className="shadow"></div> */}
+          <div className="overlay"></div>
+          <div className="blur-green"></div>
+          <img src={dynamicImage} alt="" />
+        </div>
+        <div className="blur-blue"></div>
+        <div className="elipse-linear-1"></div>
+        <div className="elipse-linear-2"></div>
+        <div className="elipse-linear-3"></div>
       </section>
+
       <div className="content">
         <section className="introduction">
           <p>Hackathon é um evento semestral de destaque em nosso calendário, especialmente voltado para <b>as
@@ -38,7 +117,7 @@ const Feed = () => {
             evento inspirador.</p>
           <div className="images">
             <div className="container-post">
-              <img src="src\assets\images-feed\img-1.png" alt="" />
+              <img src="src\assets\images-feed\img-1.png" alt="" onClick={() => openModalImage("src/assets/images-feed/img-1.png")} />
               <div className="caption">
                 <div className="likes">
                   <FaHeart />
@@ -52,7 +131,7 @@ const Feed = () => {
               </div>
             </div>
             <div className="container-post">
-              <img src="src\assets\images-feed\img-2.png" alt="" />
+              <img src="src\assets\images-feed\img-2.png" alt="" onClick={() => openModalImage("src/assets/images-feed/img-2.png")} />
               <div className="caption">
                 <div className="likes">
                   <FaHeart />
@@ -66,7 +145,7 @@ const Feed = () => {
               </div>
             </div>
             <div className="container-post">
-              <img src="src\assets\images-feed\img-3.png" alt="" />
+              <img src="src\assets\images-feed\img-3.png" alt="" onClick={() => openModalImage("src/assets/images-feed/img-3.png")} />
               <div className="caption">
                 <div className="likes">
                   <FaHeart />
@@ -162,6 +241,7 @@ const Feed = () => {
           </section>
         </section>
       </div>
+
       <footer>
         <div className="logotype-footer">
           <p>bosch</p>
@@ -174,6 +254,93 @@ const Feed = () => {
           <p>Declarações e definições de privacidade</p>
         </section>
       </footer>
+
+      {modalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>&times;</span>
+            <h1>Avaliação do evento</h1>
+
+            <div className="row">
+              <div className="modal-sub-container">
+                <h3>Nota</h3>
+                <p>Dê uma nota para o último evento que você participou</p>
+                <div className="stars">
+                  {[...Array(5)].map((_, index) => (
+                    <FaStar
+                      key={index}
+                      onClick={() => handleStarClick(index)}
+                      color={index < rating ? '#FFCD4D' : '#DFDFE0'} // Pinta as estrelas até a classificação atual
+                      size={30}
+                      style={{ cursor: 'pointer' }}
+                    />
+                  ))}
+                </div>
+
+                <h3>Pontos de destaque</h3>
+                <p>O que você mais gostou no evento?</p>
+                <div className="flags">
+                  <div className="flag-container">
+                    {['Temáticas abordadas', 'Interações sociais'].map((option, index) => (
+                      <button
+                        key={option}
+                        className={`flag-option ${selectedOptions.includes(option) ? 'active' : ''}`}
+                        onClick={() => handleOptionSelect(option)}
+                      >
+                        {option}
+                      </button>
+                    ))}
+                  </div>
+                  <div className="flag-container">
+                    {['Alimentação', 'Pontualidade', 'Outro'].map((option, index) => (
+                      <button
+                        key={option}
+                        className={`flag-option ${selectedOptions.includes(option) ? 'active' : ''}`}
+                        onClick={() => handleOptionSelect(option)}
+                      >
+                        {option}
+                      </button>
+                    ))}
+                  </div>
+                </div>
+
+                <h3>Sugestões</h3>
+                <p>Gostaria de dar alguma sugestão de melhoria (anônimo)?</p>
+                <textarea className="custom-textarea" placeholder="Digite aqui"></textarea>
+              </div>
+
+              <div className="modal-sub-container">
+                <h3>Comentários</h3>
+                <p>Gostaria de compartilhar algum comentário sobre o evento ou a plataforma
+                  com os demais membros do Esy?</p>
+                <textarea className="custom-textarea" placeholder="Digite aqui"></textarea>
+
+                <div className="check-container">
+                  <input
+                    type="checkbox"
+                    checked={isChecked}
+                    onChange={handleCheckboxChange}
+                  />
+                  <label className='modal-permission'>Autorizo a publicação do comentário e avaliação no feed principal.</label>
+                </div>
+
+                <div className="btn-modal-container">
+                  <button className="btn-submit">Enviar avaliação</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {modalOpenImage && (
+        <div className="modal-overlay-image" onClick={closeModalImage}>
+        <div className="modal-content-image">
+          <img src={selectedImage} alt="" />
+          <button className="close-button-image" onClick={closeModalImage}>Fechar</button>
+        </div>
+      </div>
+      )}
     </div>
   )
 }
