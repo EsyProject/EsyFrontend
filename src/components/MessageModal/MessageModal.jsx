2b0138d991415b28dcc6f2b46ee19bc5c1a562c0
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-04-03 16:33:24 -0300

    feat: configuring the modal structure

--- src/components/MessageModal/MessageModal.jsx
+++ src/components/MessageModal/MessageModal.jsx
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types";
 import "material-symbols";
+import "./MessageModal.css";
 
-const PopUp = ({ title, text }) => {
+const MessageModal = ({ title, text }) => {
   return (
-    <div className="popup-container">
-      <div className="popup-content">
-        <div className="popup-icon">
+    <div className="messagemodal-container">
+      <div className="overlay"></div>
+      <div className="messagemodal-content">
+        <div className="messagemodal-icon">
           <span className="material-symbols-outlined">check</span>
         </div>
         <h1>{title}</h1>
@@ -15,9 +17,9 @@ const PopUp = ({ title, text }) => {
   );
 };
 
-PopUp.propTypes = {
+MessageModal.propTypes = {
   title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
 };
 
-export default PopUp;
+export default MessageModal;
