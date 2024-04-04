8ce376a4f030cc1a2eb8272a56482947191c6c7f
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-04-04 10:00:12 -0300

    feat: adding a function to close the modal

--- src/components/MessageModal/MessageModal.jsx
+++ src/components/MessageModal/MessageModal.jsx
@@ -2,10 +2,16 @@ import PropTypes from "prop-types";
 import "material-symbols";
 import "./MessageModal.css";
 
-const MessageModal = ({ title, text }) => {
+const MessageModal = ({ title, text, onClose }) => {
+  const handleBackgroundClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
   return (
     <div className="messagemodal-container">
-      <div className="overlay"></div>
+      <div className="overlay" onClick={handleBackgroundClick}></div>
       <div className="messagemodal-content">
         <div className="messagemodal-icon">
           <span className="material-symbols-rounded">check</span>
@@ -20,6 +26,7 @@ const MessageModal = ({ title, text }) => {
 MessageModal.propTypes = {
   title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 
 export default MessageModal;
