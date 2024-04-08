621a9c6dfef8f3b6c5e3114310331b327f4319ec
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-04-08 13:50:41 -0300

    feat: using the components Navbar and Sidebar

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState } from "react";
+import { Sidebar, Navbar } from "../../pages/index";
+import "./Dashboard.css";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-export default Dashboard
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  return (
+    <div className={`historic-container ${sidebarOpen ? "sidebar-open" : ""}`}>
+      <Navbar
+        currentPageIcon="dashboard"
+        activePage="dashboard"
+        navigationText="Dashboard"
+        showNavigationTexts={false}
+      />
+
+      <Sidebar
+        activePage="dashboard"
+        isOpen={sidebarOpen}
+        toggleSidebar={toggleSidebar}
+      />
+      
+    </div>
+  );
+};
+
+export default Dashboard;
