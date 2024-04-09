3a1b0fa14f5487571f45720bfc51b232c48d9fc6
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-04-09 08:49:13 -0300

    feat: Dashboard update

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -55,64 +55,67 @@ const Dashboard = () => {
           <div className="reviews-title">
             <h2>Avaliações dos participantes</h2> <span>(88 avaliações)</span>
           </div>
-          <div className="container-reviews">
-            <div className="event-reviews">
-              <div className="event-note">
-                <div className="note-content">
-                  <h2>Nota do evento</h2>
-                  <h4>4,98</h4>
-                  <span>★</span>
-                </div>
-              </div>
-              <div className="evaluations">
-                <div className="evaluation">
-                  <div className="user-info">
-                    <div className="avatar">
-                      <h4>IR</h4>
-                    </div>
-                    <div className="user-details">
-                      <h5>Isabela Rodrigues</h5>
-                      <span>3 meses atrás</span>
-                    </div>
-                  </div>
-                  <div className="rating">★★★★★</div>
-                  <p>
-                    Evento excelente! Ótimas palestras e networking incrível.
-                  </p>
-                </div>
 
-                <div className="evaluation">
-                  <div className="user-info">
-                    <div className="avatar">
-                      <h4>LO</h4>
-                    </div>
-                    <div className="user-details">
-                      <h5>Lucas Oliveira</h5>
-                      <span>3 meses atrás</span>
-                    </div>
+          <div className="participant-reviews-main">
+            <div className="container-reviews">
+              <div className="event-reviews">
+                <div className="event-note">
+                  <div className="note-content">
+                    <h2>Nota do evento</h2>
+                    <h4>4,98</h4>
+                    <span>★</span>
                   </div>
-                  <div className="rating">★★★★★</div>
-                  <p>
-                    Adorei o evento! A organização foi impecável e aprendi muito
-                    com os workshops
-                  </p>
                 </div>
-
-                <div className="evaluation">
-                  <div className="user-info">
-                    <div className="avatar">
-                      <h4>CS</h4>
+                <div className="evaluations">
+                  <div className="evaluation">
+                    <div className="user-info">
+                      <div className="avatar">
+                        <h4>IR</h4>
+                      </div>
+                      <div className="user-details">
+                        <h5>Isabela Rodrigues</h5>
+                        <span>3 meses atrás</span>
+                      </div>
                     </div>
-                    <div className="user-details">
-                      <h5>Carolina Santos</h5>
-                      <span>3 meses atrás</span>
-                    </div>
+                    <div className="rating">★★★★★</div>
+                    <p>
+                      Evento excelente! Ótimas palestras e networking incrível.
+                    </p>
                   </div>
-                  <div className="rating">★★★★★</div>
-                  <p>
-                    Evento fantástico! As atividades foram muito envolventes e
-                    as discussões foram extremamente enriquecedoras
-                  </p>
+
+                  <div className="evaluation">
+                    <div className="user-info">
+                      <div className="avatar">
+                        <h4>LO</h4>
+                      </div>
+                      <div className="user-details">
+                        <h5>Lucas Oliveira</h5>
+                        <span>3 meses atrás</span>
+                      </div>
+                    </div>
+                    <div className="rating">★★★★★</div>
+                    <p>
+                      Adorei o evento! A organização foi impecável e aprendi
+                      muito com os workshops
+                    </p>
+                  </div>
+
+                  <div className="evaluation">
+                    <div className="user-info">
+                      <div className="avatar">
+                        <h4>CS</h4>
+                      </div>
+                      <div className="user-details">
+                        <h5>Carolina Santos</h5>
+                        <span>3 meses atrás</span>
+                      </div>
+                    </div>
+                    <div className="rating">★★★★★</div>
+                    <p>
+                      Evento fantástico! As atividades foram muito envolventes e
+                      as discussões foram extremamente enriquecedoras
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -120,32 +123,33 @@ const Dashboard = () => {
             <div className="highlights">
               <h2>Pontos de destaque</h2>
             </div>
-          </div>
-        </div>
 
-        <div className="improvement-suggestions">
-          <h2>Sugestões de melhorias</h2>
+            <div className="improvement-suggestions">
+              <h2>Sugestões de melhorias</h2>
 
-          <div className="sugestions">
-            <div className="suggestion">
-              <h5>Sugestão 1 - 10/02/2024</h5>
-              <p>
-                Seria interessante diversificar os palestrantes convidados,
-                incluindo vozes de diferentes origens e áreas de especialização.
-                Isso poderia enriquecer ainda mais o conteúdo do evento e
-                oferecer perspectivas variadas sobre os temas abordados.
-              </p>
-            </div>
+              <div className="sugestions">
+                <div className="suggestion">
+                  <h5>Sugestão 1 - 10/02/2024</h5>
+                  <p>
+                    Seria interessante diversificar os palestrantes convidados,
+                    incluindo vozes de diferentes origens e áreas de
+                    especialização. Isso poderia enriquecer ainda mais o
+                    conteúdo do evento e oferecer perspectivas variadas sobre os
+                    temas abordados.
+                  </p>
+                </div>
 
-            <div className="suggestion">
-              <h5>Sugestão 2 - 13/02/2024</h5>
-              <p>
-                Sugiro intensificar os esforços de divulgação pré-evento,
-                explorando diferentes canais de comunicação e plataformas online
-                para alcançar um público mais amplo. Isso poderia aumentar a
-                participação e garantir que o evento alcance seu potencial
-                máximo de engajamento.
-              </p>
+                <div className="suggestion">
+                  <h5>Sugestão 2 - 13/02/2024</h5>
+                  <p>
+                    Sugiro intensificar os esforços de divulgação pré-evento,
+                    explorando diferentes canais de comunicação e plataformas
+                    online para alcançar um público mais amplo. Isso poderia
+                    aumentar a participação e garantir que o evento alcance seu
+                    potencial máximo de engajamento.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
