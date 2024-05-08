7f04b56db41ce2ff08492faacb747b59ef2db8ce
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-05-08 16:03:00 -0300

    fix: making use of proptypes

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,21 +1,26 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import "./Logout.css";
 
 const Logout = ({ onClose }) => {
-    return (
-        <div className="modal-logout">
-            <div className="modal-logout-content">
-                <h1>Log out</h1>
-                <p>Você está indo embora... Tem certeza?</p>
+  return (
+    <div className="modal-logout">
+      <div className="modal-logout-content">
+        <h1>Log out</h1>
+        <p>Você está indo embora... Tem certeza?</p>
 
-                <div className="button-container">
-                    <button className="exit-button">Sim, desconectar</button>
-                    <button className="custom-button" onClick={onClose}>Não, quero ficar</button>
-                </div>
-            </div>
+        <div className="button-container">
+          <button className="exit-button">Sim, desconectar</button>
+          <button className="custom-button" onClick={onClose}>
+            Não, quero ficar
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
+};
+
+Logout.propTypes = {
+  onClose: PropTypes.func.isRequired,
 };
 
 export default Logout;
