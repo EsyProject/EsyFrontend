288c5d4cba1a099997494429176ffdd668aa23cb
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-05-14 16:51:34 -0300

    feat: adding the logout function using sso to the Logout component

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,7 +1,18 @@
 import PropTypes from "prop-types";
+import { useMsal } from "@azure/msal-react";
 import "./Logout.css";
 
 const Logout = ({ onClose }) => {
+  const { instance } = useMsal();
+
+  const handleLogout = (logoutType) => {
+    if (logoutType === "popup") {
+      instance.logoutPopup();
+    } else if (logoutType === "redirect") {
+      instance.logoutRedirect();
+    }
+  };
+
   return (
     <div className="modal-logout">
       <div className="modal-logout-content">
@@ -9,7 +20,7 @@ const Logout = ({ onClose }) => {
         <p>Você está indo embora... Tem certeza?</p>
 
         <div className="button-container">
-          <button className="exit-button">Sim, desconectar</button>
+          <button className="exit-button" onClick={() => handleLogout("popup")} color="inherit">Sim, desconectar</button>
           <button className="custom-button" onClick={onClose}>
             Não, quero ficar
           </button>
