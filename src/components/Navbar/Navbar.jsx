15007f228ea3143c93fe46dc72e954025d0d829e
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-05-15 14:50:08 -0300

    feat: adding the name and image of the connected user to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,15 @@
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+
+import {
+  InteractionRequiredAuthError,
+  InteractionStatus,
+} from "@azure/msal-browser";
+import { useMsal } from "@azure/msal-react";
+import { callMsGraph } from "../../lib/sso/MsGraphApiCalls";
+import { loginRequest } from "../../lib/sso/authConfig";
 
 const Navbar = ({
   currentPageIcon,
@@ -9,34 +18,69 @@ const Navbar = ({
   navigationText,
   tabs,
 }) => {
+  const [imageUrl, setImageUrl] = useState("");
+
+  const { instance, inProgress } = useMsal();
+  const account = instance.getActiveAccount();
+
+  useEffect(() => {
+    if (!imageUrl && inProgress === InteractionStatus.None) {
+      callMsGraph()
+        .then((response) => {
+          setImageUrl(response?.blobUrl);
+        })
+        .catch((e) => {
+          if (e instanceof InteractionRequiredAuthError) {
+            instance.acquireTokenRedirect({
+              ...loginRequest,
+              account: instance.getActiveAccount(),
+            });
+          }
+        });
+    }
+  }, [inProgress, instance, imageUrl, account?.name]);
+
   return (
     <div className="navbar-container">
       <div className="navbar-content">
-        {/* Container for icon */}
-        <div className="rectangle">
-          <span className="material-symbols-outlined">{currentPageIcon}</span>
-        </div>
+        <section className="navbar-content-child">
+          {/* Container for icon */}
+          <div className="rectangle">
+            <span className="material-symbols-outlined">{currentPageIcon}</span>
+          </div>
 
-        {/* Navigation text */}
-        <h2>{navigationText}</h2>
+          {/* Navigation text */}
+          <h2>{navigationText}</h2>
 
-        {/* Rendering of tabs */}
-        {tabs && tabs.length > 0 && (
-          tabs.map((tab, index) => (
-            <Link key={index} to={tab.link} className={activePage === tab.name ? "active" : ""}>
-              {showNavigationTexts && (
-                <h4 className={activePage === tab.name ? "active" : ""}>
-                  {tab.text}
-                </h4>
-              )}
-              {activePage === tab.name && <div className={`subtitle-underline-${tab.name}`}></div>}
-            </Link>
-          ))
-        )}
-      </div>
-      {/* Container for notification icon */}
-      <div className="navbar-icon">
-        <span className="material-symbols-outlined">notifications</span>
+          {/* Rendering of tabs */}
+          {tabs &&
+            tabs.length > 0 &&
+            tabs.map((tab, index) => (
+              <Link
+                key={index}
+                to={tab.link}
+                className={activePage === tab.name ? "active" : ""}
+              >
+                {showNavigationTexts && (
+                  <h4 className={activePage === tab.name ? "active" : ""}>
+                    {tab.text}
+                  </h4>
+                )}
+                {activePage === tab.name && (
+                  <div className={`subtitle-underline-${tab.name}`}></div>
+                )}
+              </Link>
+            ))}
+        </section>
+
+        <section className="navbar-content-child">
+          <div className="user-data navbar-content-child">
+            <h2>{account?.name}</h2>
+            <div className="profile">
+              <img src={imageUrl} alt="img-profile" className="img-profile" />
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
