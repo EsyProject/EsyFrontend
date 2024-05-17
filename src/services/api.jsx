550f14f8efed2bd710603af0849299b4bd280002
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-05-17 11:16:52 -0300

    feat: configuring post interaction with the api

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,8 +1,29 @@
-import axiosInstance from './axiosInstance';
+// definition of how to interact with the API
+import axiosInstance from "./axiosInstance";
 
-// Event 
+// Event
+export const createEvent = async (data) => {
+  const formData = new FormData();
+
+  Object.keys(data).forEach((key) => {
+    if (key === "image") {
+      formData.append(key, data[key]);
+    } else {
+      formData.append(key, data[key]);
+    }
+  });
+
+  return (
+    await axiosInstance.post("/event", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+  ).data;
+};
+
 export const getAllEvents = async () => {
-  return (await axiosInstance.get('/event/events')).data;
+  return (await axiosInstance.get("/event/events")).data;
 };
 
 export const getEventById = async (eventId) => {
@@ -10,22 +31,26 @@ export const getEventById = async (eventId) => {
 };
 
 export const getEventName = async () => {
-  return (await axiosInstance.get('/event/name')).data;
+  return (await axiosInstance.get("/event/name")).data;
 };
 
 export const getMyEvents = async () => {
-  return (await axiosInstance.get('/event/myEvent')).data;
+  return (await axiosInstance.get("/event/myEvent")).data;
 };
 
 export const getEventFeed = async () => {
-  return (await axiosInstance.get('/event/feed')).data;
+  return (await axiosInstance.get("/event/feed")).data;
 };
 
 export const getCardEvent = async () => {
-  return (await axiosInstance.get('/event/card')).data;
+  return (await axiosInstance.get("/event/card")).data;
 };
 
-// Assessment 
+// Assessment
+export const createAssessment = async (eventId, data) => {
+  return (await axiosInstance.post(`/assessment/${eventId}`, data)).data;
+};
+
 export const getAssessmentWithEvent = async (eventId) => {
   return (await axiosInstance.get(`/assessment/assessments/${eventId}`)).data;
 };
@@ -46,4 +71,15 @@ export const getHighPointsGraph = async (eventId) => {
 
 export const getSuggestionsBasedOnAssessment = async (eventId) => {
   return (await axiosInstance.get(`/dashboard/suggestion/${eventId}`)).data;
+};
+
+// Ticket Event
+export const createTicket = async (data) => {
+  return (await axiosInstance.post("/ticket", data)).data;
+};
+
+export const updateTicket = async (ticketId, data) => {
+  return (
+    await axiosInstance.patch(`/ticket/${ticketId}`, data)
+  ).data;
 };
