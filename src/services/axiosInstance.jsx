61622f631e1fa93d9b9c7ce81aded81c9afd750b
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-05-17 11:17:21 -0300

    feat: configuring the headers

--- src/services/axiosInstance.jsx
+++ src/services/axiosInstance.jsx
@@ -1,11 +1,20 @@
 import axios from "axios";
 import { msalInstance } from "../lib/sso/msalInstance";
  
+export const headers = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  "Access-Control-Allow-Origin": "true",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+}; 
+
 const API_URL = "http://10.234.81.170:6968";
  
 // Defining the URL instance for requests using AXIOS
 const instance = axios.create({
   baseURL: API_URL,
+  headers: headers
 });
  
 // Function to acquire the token and add it to the request header
