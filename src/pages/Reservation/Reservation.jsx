c8dbc8f21882293cde46264b0ffbee71fff94eb9
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-05-28 16:57:11 -0300

    fix: posting the ticket

--- src/pages/Reservation/Reservation.jsx
+++ src/pages/Reservation/Reservation.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
-import { Sidebar, Navbar, EventCard, /* ReaderQR */ } from "../../components/index";
+import { useState, useEffect } from "react";
+import { Sidebar, Navbar, EventCard } from "../../components/index";
 import AreaModal from "../../components/AreaModal/AreaModal";
 import Footer from "../../components/Footer/Footer";
-import "./Reservation.css"
-
-// apenas para fins de teste
+import "./Reservation.css";
+import { useEventById } from "../../services/queries";
+import { useGetTicket } from "../../services/mutations";
 import Carousel from "../../components/Carousel/Carousel";
 
 const Reservation = () => {
+  const eventId = "2";
+  const { data: eventFeed } = useEventById(eventId);
+
+  const [eventFeedData, setEventFeedData] = useState(null);
+
   const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  useEffect(() => {
+    if (eventFeed) {
+      setEventFeedData(eventFeed);
+    }
+  }, [eventFeed]);
+
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
@@ -89,6 +101,22 @@ const Reservation = () => {
     },
   ];
 
+  const { nameOfEvent, responsible_area, localEvent, description, initialTime, finishTime, initialDate } = eventFeedData || {};
+
+  const displayResponsibleArea = responsible_area === "ETS" ? "ETS - Engineering Technical School" : responsible_area;
+
+  // Função para formatar a hora
+  const formatTime = (time) => {
+    if (!time) return "";
+    return time.slice(0, 5);
+  };
+
+  const { mutate: getTicket } = useGetTicket();
+
+  const handleParticipateClick = () => {
+    getTicket(eventId);
+  };
+
   return (
     <div className={`feed-container ${sidebarOpen ? "sidebar-open" : ""}`}>
       <Navbar
@@ -111,27 +139,20 @@ const Reservation = () => {
       <div className="container-main container-main-local">
         <div className="container-child">
           <EventCard
-            audience="Público Principal - Digital Solutions e Mecatrônica"
-            title="hackaton"
+            audience={displayResponsibleArea}
+            title={nameOfEvent}
             subtitle="7ª Edição"
-            date="10/07/2024"
-            time="09h00"
-            location="Ca300"
+            date={initialDate}
+            time={formatTime(initialTime)}
+            location={localEvent}
           />
 
           <div className="description-content">
             <div className="description-row-title">
-              <h1 className="description-title">Hackathon 7ª Edição</h1>
+              <h1 className="description-title">{nameOfEvent} 7ª Edição</h1>
             </div>
 
-            <p className="description-content-text description-main">Hackathon é nosso evento semestral focado nas áreas
-              de tecnologia e inovação e conta com os aprendizes dos cursos de Digital Solutions e
-              Mecatrônica. Durante uma semana os aprendizes desenvolvem projetos reais que buscam
-              trazer melhorias internas para algum processo ou setor da Bosch Campinas. Durante
-              este período as equipes apredem de forma muito rápida e eficiente a trabalharem de
-              modo ágil e exploram sua criatividade e capacidades técnicas e socioemocionais.
-              Venha participar da apresentação dos projetos e descobrir pessoalmente o quão
-              talentosos são os nossos jovens.</p>
+            <p className="description-content-text description-main">{description}</p>
 
             <div className="description-infos">
               <div className="description-wrapper">
@@ -139,27 +160,25 @@ const Reservation = () => {
                   <span className="material-symbols-outlined">
                     calendar_month
                   </span>
-                  <p>10/07/2024</p>
+                  <p>{initialDate}</p>
                 </div>
 
                 <div className="description-date">
                   <span className="material-symbols-outlined">schedule</span>
-                  <p>09:00 às 15:00</p>
+                  <p>{formatTime(initialTime)} às {formatTime(finishTime)}</p>
                 </div>
 
                 <div className="description-date">
                   <span className="material-symbols-outlined">location_on</span>
-                  <p>Ca300</p>
+                  <p>{localEvent}</p>
                 </div>
               </div>
             </div>
 
             <div className="btn-container">
-              <button className="btn-primary">Participar</button>
-              {/* link para acessar informações da área */}
+              <button onClick={handleParticipateClick} className="btn-primary">Participar</button>
               <button onClick={openModalArea} className="btn-link">Saiba mais sobre a área do evento</button>
               {isModalAreaOpen && <AreaModal onClose={closeModalArea} />}
-              {/* <ReaderQR /> */}
             </div>
           </div>
         </div>
@@ -171,7 +190,6 @@ const Reservation = () => {
           <Footer />
         </div>
       </div>
-
     </div>
   );
 };
