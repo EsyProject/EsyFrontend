6a9881b3c687f712d08cf7f5129013c3d41e5748
rochamanuela <rochadesouzamanuela@gmail.com>
2024-06-03 16:11:39 -0300

    style: adjusts in card view

--- src/components/CardView/CardView.jsx
+++ src/components/CardView/CardView.jsx
@@ -4,7 +4,11 @@ import "material-symbols";
 import "./CardView.css";
 import CardViewModal from "../CardViewModal/CardViewModal";
 
-const CardView = ({ date, title, description, area, img }) => {
+// testeee
+
+import { format, addDays } from 'date-fns';
+
+const CardView = ({ nameOfEvent, finishDate, finishTime, initialDate, initialTime, localEvent, responsible_area, description, img }) => {
 
   // estado do modal de descrição
   const [isCardViewModalOpen, setIsCardViewModalOpen] = useState(false);
@@ -24,22 +28,35 @@ const CardView = ({ date, title, description, area, img }) => {
     setIsCardViewModalOpen(false);
   };
 
+  // teste de novo
+
+  const formatDate = (date) => {
+    const adjustedDate = addDays(new Date(date), 1);
+
+    return format(adjustedDate, 'yyyy-MM-dd');
+  };
+
+  const formatTime = (time) => {
+    return format(new Date(`1970-01-01T${time}:00`), 'HH:mm:ss');
+  };
+
+
   return (
     <div className="simple-event-card-view">
-      <div className="rectangle-event" data-area={area}></div>
+      <div className="rectangle-event" data-area={responsible_area}></div>
       <div className="event-details">
         <div className="date-area-card-view">
           <div className="date-icon">
             <span className="material-symbols-outlined">calendar_today</span>
-            <p>{date}</p>
+            <p>{initialDate}</p>
           </div>
 
-          <div className="event-area" data-area={area}>
-            {area}
+          <div className="event-area" data-area={responsible_area}>
+            {responsible_area}
           </div>
         </div>
 
-        <h4>{title}</h4>
+        <h4>{nameOfEvent}</h4>
         <p className="event-description">{minDescription}</p>
       </div>
 
@@ -47,20 +64,48 @@ const CardView = ({ date, title, description, area, img }) => {
         <span className="material-symbols-rounded">pan_zoom</span>
       </button>
 
-      {/* teste */}
-
       {isCardViewModalOpen && (
         <CardViewModal onClose={handleCloseCardViewModal}>
-          <div className="card-view-modal-container-main">
-            <div className="card-view-modal-content-local">
+          <div className="card-view-modal-content">
+            <div className="card-view-modal-content-child">
               <img src={img} />
             </div>
-            <div className="card-view-modal-content">
-              <p>{area}</p>
-              <h4>{title}</h4>
-              <p>{date}</p>
-              <p>{description}</p>
-              <button onClick={handleCloseCardViewModal}>Fechar</button>
+            <div className="card-view-modal-content-child">
+              <div className="child">
+                <div className="card-btn-container">
+                  <button onClick={handleCloseCardViewModal} className="card-view-modal-btn edit">
+                    <span className="material-symbols-rounded">stylus</span>
+                    <p>Editar informações do evento</p>
+                  </button>
+                  <button onClick={handleCloseCardViewModal} className="card-view-modal-btn clos">
+                  <span className="material-symbols-rounded">close</span>
+                    <p>Fechar</p>
+                  </button>
+                </div>
+                <div className="flag-area">
+                  <div className="event-area" data-area="ETS - DS">ETS - DS</div>
+                </div>
+                <h1>{nameOfEvent}</h1>
+                <div className="info-wrapper">
+                  <div className="info-date">
+                    <span className="material-symbols-outlined">
+                      calendar_month
+                    </span>
+                    <p>{initialDate}</p>
+                  </div>
+
+                  <div className="info-date">
+                    <span className="material-symbols-outlined">schedule</span>
+                    <p>{formatTime(initialTime)} às {formatTime(finishTime)}</p>
+                  </div>
+
+                  <div className="info-date">
+                    <span className="material-symbols-outlined">location_on</span>
+                    <p>{localEvent}</p>
+                  </div>
+                </div>
+                <p className="description">{description}</p>
+              </div>
             </div>
           </div>
         </CardViewModal>
@@ -70,11 +115,14 @@ const CardView = ({ date, title, description, area, img }) => {
 };
 
 CardView.propTypes = {
-  date: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
+  nameOfEvent: PropTypes.string.isRequired,
+  finishDate: PropTypes.string.isRequired,
+  finishTime: PropTypes.string.isRequired,
+  initialDate: PropTypes.string.isRequired,
+  initialTime: PropTypes.string.isRequired,
+  localEvent: PropTypes.string.isRequired,
+  responsible_area: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  area: PropTypes.string.isRequired,
-  // img: PropTypes.string.isRequired,
 };
 
 export default CardView;
