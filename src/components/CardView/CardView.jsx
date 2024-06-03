522e8b090756ffb0091043c9bb5b7e2506bccb29
rochamanuela <rochadesouzamanuela@gmail.com>
2024-06-03 16:51:12 -0300

    feat: all events screen in adm side, finalized

--- src/components/CardView/CardView.jsx
+++ src/components/CardView/CardView.jsx
@@ -3,17 +3,11 @@ import PropTypes from "prop-types";
 import "material-symbols";
 import "./CardView.css";
 import CardViewModal from "../CardViewModal/CardViewModal";
-
-// testeee
-
 import { format, addDays } from 'date-fns';
 
 const CardView = ({ nameOfEvent, finishDate, finishTime, initialDate, initialTime, localEvent, responsible_area, description, img }) => {
-
-  // estado do modal de descrição
   const [isCardViewModalOpen, setIsCardViewModalOpen] = useState(false);
 
-  // fazer com que o texto não apaeça inteiro no card
   const maxLength = 150;
   const minDescription =
     description.length > maxLength
@@ -28,18 +22,14 @@ const CardView = ({ nameOfEvent, finishDate, finishTime, initialDate, initialTim
     setIsCardViewModalOpen(false);
   };
 
-  // teste de novo
-
   const formatDate = (date) => {
     const adjustedDate = addDays(new Date(date), 1);
-
     return format(adjustedDate, 'yyyy-MM-dd');
   };
 
   const formatTime = (time) => {
     return format(new Date(`1970-01-01T${time}:00`), 'HH:mm:ss');
   };
-
 
   return (
     <div className="simple-event-card-view">
@@ -50,16 +40,13 @@ const CardView = ({ nameOfEvent, finishDate, finishTime, initialDate, initialTim
             <span className="material-symbols-outlined">calendar_today</span>
             <p>{initialDate}</p>
           </div>
-
           <div className="event-area" data-area={responsible_area}>
             {responsible_area}
           </div>
         </div>
-
         <h4>{nameOfEvent}</h4>
         <p className="event-description">{minDescription}</p>
       </div>
-
       <button className="card-view-btn" onClick={handleOpenCardViewModal}>
         <span className="material-symbols-rounded">pan_zoom</span>
       </button>
@@ -68,7 +55,7 @@ const CardView = ({ nameOfEvent, finishDate, finishTime, initialDate, initialTim
         <CardViewModal onClose={handleCloseCardViewModal}>
           <div className="card-view-modal-content">
             <div className="card-view-modal-content-child">
-              <img src={img} />
+              <img src={img} alt="Event" />
             </div>
             <div className="card-view-modal-content-child">
               <div className="child">
@@ -78,27 +65,23 @@ const CardView = ({ nameOfEvent, finishDate, finishTime, initialDate, initialTim
                     <p>Editar informações do evento</p>
                   </button>
                   <button onClick={handleCloseCardViewModal} className="card-view-modal-btn clos">
-                  <span className="material-symbols-rounded">close</span>
+                    <span className="material-symbols-rounded">close</span>
                     <p>Fechar</p>
                   </button>
                 </div>
                 <div className="flag-area">
-                  <div className="event-area" data-area="ETS - DS">ETS - DS</div>
+                  <div className="event-area" data-area={responsible_area}>{responsible_area}</div>
                 </div>
                 <h1>{nameOfEvent}</h1>
                 <div className="info-wrapper">
                   <div className="info-date">
-                    <span className="material-symbols-outlined">
-                      calendar_month
-                    </span>
+                    <span className="material-symbols-outlined">calendar_month</span>
                     <p>{initialDate}</p>
                   </div>
-
                   <div className="info-date">
                     <span className="material-symbols-outlined">schedule</span>
                     <p>{formatTime(initialTime)} às {formatTime(finishTime)}</p>
                   </div>
-
                   <div className="info-date">
                     <span className="material-symbols-outlined">location_on</span>
                     <p>{localEvent}</p>
@@ -123,6 +106,7 @@ CardView.propTypes = {
   localEvent: PropTypes.string.isRequired,
   responsible_area: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
 };
 
 export default CardView;
--- src/pages/AllEvents/AllEvents.jsx
+++ src/pages/AllEvents/AllEvents.jsx
@@ -234,6 +234,7 @@ const AllEvents = () => {
       />
 
       <div className="all-events-container-main">
+        <p className="all-events-title">Todos os eventos</p>
         <div className="all-events-content">
           <CardView
             nameOfEvent="Jornada do Conhecimento"
@@ -272,7 +273,67 @@ const AllEvents = () => {
             initialTime="10:00"
             finishTime="14:00"
             localEvent="Ca300"
-            responsible_area="ETS - DS"
+            responsible_area="BD - DEV"
+
+            description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
+            quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
+            pariatur dolore in quae eveniet? Et dolore asperiores aut nostrum 
+            necessitatibus ex error modi ut veniam dolore 33 omnis error sed 
+            molestias velit qui blanditiis odio. Ut galisum animi et assumenda 
+            assumenda aut itaque maxime ut soluta incidunt non labore totam qui 
+            doloribus soluta ut suscipit galisum. Et aliquam rerum eum accusantium 
+            dignissimos et quia minima. Est recusandae placeat At officiis ipsa ut 
+            exercitationem ratione ea exercitationem eaque eos natus enim.
+
+            Est incidunt dignissimos ut veritatis blanditiis sit nesciunt earum et 
+            nihil commodi id repellendus molestias. Et voluptatibus laborum ex labore 
+            cupiditate et minus voluptas. Aut labore recusandae et commodi blanditiis 
+            et dolorum sint sed voluptatem quasi. Et sequi libero aut quibusdam autem 
+            ut facilis voluptate vel quas autem rem Quis sunt et beatae officia. 
+            Ut sequi eligendi qui voluptas molestias id voluptatibus dolore ea 
+            magnam aliquam."
+
+            img="src/assets/dog.jpg"
+          />
+
+          <CardView
+            nameOfEvent="Jornada do Conhecimento"
+            initialDate="22/02/2024"
+            finishDate="26/02/2024"
+            initialTime="10:00"
+            finishTime="14:00"
+            localEvent="Ca300"
+            responsible_area="AMB - GE"
+
+            description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
+            quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
+            pariatur dolore in quae eveniet? Et dolore asperiores aut nostrum 
+            necessitatibus ex error modi ut veniam dolore 33 omnis error sed 
+            molestias velit qui blanditiis odio. Ut galisum animi et assumenda 
+            assumenda aut itaque maxime ut soluta incidunt non labore totam qui 
+            doloribus soluta ut suscipit galisum. Et aliquam rerum eum accusantium 
+            dignissimos et quia minima. Est recusandae placeat At officiis ipsa ut 
+            exercitationem ratione ea exercitationem eaque eos natus enim.
+
+            Est incidunt dignissimos ut veritatis blanditiis sit nesciunt earum et 
+            nihil commodi id repellendus molestias. Et voluptatibus laborum ex labore 
+            cupiditate et minus voluptas. Aut labore recusandae et commodi blanditiis 
+            et dolorum sint sed voluptatem quasi. Et sequi libero aut quibusdam autem 
+            ut facilis voluptate vel quas autem rem Quis sunt et beatae officia. 
+            Ut sequi eligendi qui voluptas molestias id voluptatibus dolore ea 
+            magnam aliquam."
+
+            img="src/assets/dog.jpg"
+          />
+
+          <CardView
+            nameOfEvent="Jornada do Conhecimento"
+            initialDate="22/02/2024"
+            finishDate="26/02/2024"
+            initialTime="10:00"
+            finishTime="14:00"
+            localEvent="Ca300"
+            responsible_area="HR - GE"
 
             description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
             quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
@@ -332,7 +393,7 @@ const AllEvents = () => {
             initialTime="10:00"
             finishTime="14:00"
             localEvent="Ca300"
-            responsible_area="ETS - DS"
+            responsible_area="HR - GE"
 
             description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
             quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
@@ -362,7 +423,7 @@ const AllEvents = () => {
             initialTime="10:00"
             finishTime="14:00"
             localEvent="Ca300"
-            responsible_area="ETS - DS"
+            responsible_area="BD - UX/UI"
 
             description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
             quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
@@ -392,7 +453,7 @@ const AllEvents = () => {
             initialTime="10:00"
             finishTime="14:00"
             localEvent="Ca300"
-            responsible_area="ETS - DS"
+            responsible_area="AMB - DS"
 
             description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
             quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
@@ -415,35 +476,6 @@ const AllEvents = () => {
             img="src/assets/dog.jpg"
           />
           
-          <CardView
-            nameOfEvent="Jornada do Conhecimento"
-            initialDate="22/02/2024"
-            finishDate="26/02/2024"
-            initialTime="10:00"
-            finishTime="14:00"
-            localEvent="Ca300"
-            responsible_area="ETS - DS"
-
-            description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
-            quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
-            pariatur dolore in quae eveniet? Et dolore asperiores aut nostrum 
-            necessitatibus ex error modi ut veniam dolore 33 omnis error sed 
-            molestias velit qui blanditiis odio. Ut galisum animi et assumenda 
-            assumenda aut itaque maxime ut soluta incidunt non labore totam qui 
-            doloribus soluta ut suscipit galisum. Et aliquam rerum eum accusantium 
-            dignissimos et quia minima. Est recusandae placeat At officiis ipsa ut 
-            exercitationem ratione ea exercitationem eaque eos natus enim.
-
-            Est incidunt dignissimos ut veritatis blanditiis sit nesciunt earum et 
-            nihil commodi id repellendus molestias. Et voluptatibus laborum ex labore 
-            cupiditate et minus voluptas. Aut labore recusandae et commodi blanditiis 
-            et dolorum sint sed voluptatem quasi. Et sequi libero aut quibusdam autem 
-            ut facilis voluptate vel quas autem rem Quis sunt et beatae officia. 
-            Ut sequi eligendi qui voluptas molestias id voluptatibus dolore ea 
-            magnam aliquam."
-
-            img="src/assets/dog.jpg"
-          />
         </div>
       </div>
     </div>
