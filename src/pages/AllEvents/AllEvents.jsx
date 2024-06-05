0bfce3b46ffd997023aa8a265eb58d771241dd17
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-06-05 16:55:08 -0300

    fix: displaying events

--- src/pages/AllEvents/AllEvents.jsx
+++ src/pages/AllEvents/AllEvents.jsx
@@ -1,20 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import 'react-toastify/dist/ReactToastify.css';
 import {
   Sidebar,
   Navbar
 } from "../../components/index";
 import "material-symbols";
+import { useEventById } from "../../services/queries";
 import "./AllEvents.css";
 
 import CardView from "../../components/CardView/CardView";
 
 const AllEvents = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const eventId = "18";
+  const { data: eventFeed } = useEventById(eventId);
+  const [eventFeedData, setEventFeedData] = useState(null);
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
+
+  useEffect(() => {
+    if (eventFeed) {
+      setEventFeedData(eventFeed);
+    }
+  }, [eventFeed]);
+
+  const { nameOfEvent, initialDate, responsible_area, localEvent, description, imgUrl } = eventFeedData || {};
+
+  console.log("Name of Event:", nameOfEvent);
+  console.log("Initial Date:", initialDate);
+  console.log("Responsible Area:", responsible_area);
+  console.log("Local:", localEvent);
+  console.log("Description:", description);
+  console.log("Image URL:", imgUrl);
 
   return (
     <div className={`creation-container ${sidebarOpen ? "sidebar-open" : ""}`}>
@@ -53,31 +72,15 @@ const AllEvents = () => {
         <p className="all-events-title">Todos os eventos</p>
         <div className="all-events-content">
           <CardView
-            nameOfEvent="Jornada do Conhecimento"
-            initialDate="22/02/2024"
+            nameOfEvent={nameOfEvent}
+            initialDate={initialDate}
             finishDate="26/02/2024"
             initialTime="10:00"
             finishTime="14:00"
-            localEvent="Ca300"
-            responsible_area="ETS - DS"
+            localEvent={localEvent}
+            responsible_area={responsible_area}
 
-            description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
-            quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
-            pariatur dolore in quae eveniet? Et dolore asperiores aut nostrum 
-            necessitatibus ex error modi ut veniam dolore 33 omnis error sed 
-            molestias velit qui blanditiis odio. Ut galisum animi et assumenda 
-            assumenda aut itaque maxime ut soluta incidunt non labore totam qui 
-            doloribus soluta ut suscipit galisum. Et aliquam rerum eum accusantium 
-            dignissimos et quia minima. Est recusandae placeat At officiis ipsa ut 
-            exercitationem ratione ea exercitationem eaque eos natus enim.
-
-            Est incidunt dignissimos ut veritatis blanditiis sit nesciunt earum et 
-            nihil commodi id repellendus molestias. Et voluptatibus laborum ex labore 
-            cupiditate et minus voluptas. Aut labore recusandae et commodi blanditiis 
-            et dolorum sint sed voluptatem quasi. Et sequi libero aut quibusdam autem 
-            ut facilis voluptate vel quas autem rem Quis sunt et beatae officia. 
-            Ut sequi eligendi qui voluptas molestias id voluptatibus dolore ea 
-            magnam aliquam."
+            description="{description}"
 
             img="src/assets/dog.jpg"
           />
@@ -291,7 +294,7 @@ const AllEvents = () => {
 
             img="src/assets/dog.jpg"
           />
-          
+
         </div>
       </div>
     </div>
