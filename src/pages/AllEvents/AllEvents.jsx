15337f09cbcfc8be629726ffe4f0ac685a082810
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-06-06 11:08:02 -0300

    fix: using the allevents endpoint to display all events

--- src/pages/AllEvents/AllEvents.jsx
+++ src/pages/AllEvents/AllEvents.jsx
@@ -1,39 +1,49 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useAllEvents } from "../../services/queries";
 import 'react-toastify/dist/ReactToastify.css';
 import {
   Sidebar,
   Navbar
 } from "../../components/index";
 import "material-symbols";
-import { useEventById } from "../../services/queries";
 import "./AllEvents.css";
 
 import CardView from "../../components/CardView/CardView";
 
 const AllEvents = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const eventId = "18";
-  const { data: eventFeed } = useEventById(eventId);
-  const [eventFeedData, setEventFeedData] = useState(null);
+  const { data: eventsData, isLoading, isError } = useAllEvents();
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
 
   useEffect(() => {
-    if (eventFeed) {
-      setEventFeedData(eventFeed);
-    }
-  }, [eventFeed]);
+    console.log("Events data:", eventsData);
+  }, [eventsData]);
 
-  const { nameOfEvent, initialDate, responsible_area, localEvent, description, imgUrl } = eventFeedData || {};
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-  console.log("Name of Event:", nameOfEvent);
-  console.log("Initial Date:", initialDate);
-  console.log("Responsible Area:", responsible_area);
-  console.log("Local:", localEvent);
-  console.log("Description:", description);
-  console.log("Image URL:", imgUrl);
+  if (isError) {
+    return <div>Error loading events: {isError.message}</div>;
+  }
+
+  const eventCards = eventsData.content.map((event, index) => (
+    <CardView
+      key={index}
+      nameOfEvent={event.nameOfEvent}
+      initialDate={event.initialDate}
+      finishDate={event.finishDate}
+      initialTime={event.initialTime}
+      finishTime={event.finishTime}
+      localEvent={event.localEvent}
+      responsible_area={event.responsible_area}
+      description={event.description}
+      img={event.imgUrl[0]}
+    />
+  ));
 
   return (
     <div className={`creation-container ${sidebarOpen ? "sidebar-open" : ""}`}>
@@ -70,232 +80,7 @@ const AllEvents = () => {
 
       <div className="all-events-container-main">
         <p className="all-events-title">Todos os eventos</p>
-        <div className="all-events-content">
-          <CardView
-            nameOfEvent={nameOfEvent}
-            initialDate={initialDate}
-            finishDate="26/02/2024"
-            initialTime="10:00"
-            finishTime="14:00"
-            localEvent={localEvent}
-            responsible_area={responsible_area}
-
-            description="{description}"
-
-            img="src/assets/dog.jpg"
-          />
-
-          <CardView
-            nameOfEvent="Jornada do Conhecimento"
-            initialDate="22/02/2024"
-            finishDate="26/02/2024"
-            initialTime="10:00"
-            finishTime="14:00"
-            localEvent="Ca300"
-            responsible_area="BD - DEV"
-
-            description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
-            quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
-            pariatur dolore in quae eveniet? Et dolore asperiores aut nostrum 
-            necessitatibus ex error modi ut veniam dolore 33 omnis error sed 
-            molestias velit qui blanditiis odio. Ut galisum animi et assumenda 
-            assumenda aut itaque maxime ut soluta incidunt non labore totam qui 
-            doloribus soluta ut suscipit galisum. Et aliquam rerum eum accusantium 
-            dignissimos et quia minima. Est recusandae placeat At officiis ipsa ut 
-            exercitationem ratione ea exercitationem eaque eos natus enim.
-
-            Est incidunt dignissimos ut veritatis blanditiis sit nesciunt earum et 
-            nihil commodi id repellendus molestias. Et voluptatibus laborum ex labore 
-            cupiditate et minus voluptas. Aut labore recusandae et commodi blanditiis 
-            et dolorum sint sed voluptatem quasi. Et sequi libero aut quibusdam autem 
-            ut facilis voluptate vel quas autem rem Quis sunt et beatae officia. 
-            Ut sequi eligendi qui voluptas molestias id voluptatibus dolore ea 
-            magnam aliquam."
-
-            img="src/assets/dog.jpg"
-          />
-
-          <CardView
-            nameOfEvent="Jornada do Conhecimento"
-            initialDate="22/02/2024"
-            finishDate="26/02/2024"
-            initialTime="10:00"
-            finishTime="14:00"
-            localEvent="Ca300"
-            responsible_area="AMB - GE"
-
-            description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
-            quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
-            pariatur dolore in quae eveniet? Et dolore asperiores aut nostrum 
-            necessitatibus ex error modi ut veniam dolore 33 omnis error sed 
-            molestias velit qui blanditiis odio. Ut galisum animi et assumenda 
-            assumenda aut itaque maxime ut soluta incidunt non labore totam qui 
-            doloribus soluta ut suscipit galisum. Et aliquam rerum eum accusantium 
-            dignissimos et quia minima. Est recusandae placeat At officiis ipsa ut 
-            exercitationem ratione ea exercitationem eaque eos natus enim.
-
-            Est incidunt dignissimos ut veritatis blanditiis sit nesciunt earum et 
-            nihil commodi id repellendus molestias. Et voluptatibus laborum ex labore 
-            cupiditate et minus voluptas. Aut labore recusandae et commodi blanditiis 
-            et dolorum sint sed voluptatem quasi. Et sequi libero aut quibusdam autem 
-            ut facilis voluptate vel quas autem rem Quis sunt et beatae officia. 
-            Ut sequi eligendi qui voluptas molestias id voluptatibus dolore ea 
-            magnam aliquam."
-
-            img="src/assets/dog.jpg"
-          />
-
-          <CardView
-            nameOfEvent="Jornada do Conhecimento"
-            initialDate="22/02/2024"
-            finishDate="26/02/2024"
-            initialTime="10:00"
-            finishTime="14:00"
-            localEvent="Ca300"
-            responsible_area="HR - GE"
-
-            description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
-            quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
-            pariatur dolore in quae eveniet? Et dolore asperiores aut nostrum 
-            necessitatibus ex error modi ut veniam dolore 33 omnis error sed 
-            molestias velit qui blanditiis odio. Ut galisum animi et assumenda 
-            assumenda aut itaque maxime ut soluta incidunt non labore totam qui 
-            doloribus soluta ut suscipit galisum. Et aliquam rerum eum accusantium 
-            dignissimos et quia minima. Est recusandae placeat At officiis ipsa ut 
-            exercitationem ratione ea exercitationem eaque eos natus enim.
-
-            Est incidunt dignissimos ut veritatis blanditiis sit nesciunt earum et 
-            nihil commodi id repellendus molestias. Et voluptatibus laborum ex labore 
-            cupiditate et minus voluptas. Aut labore recusandae et commodi blanditiis 
-            et dolorum sint sed voluptatem quasi. Et sequi libero aut quibusdam autem 
-            ut facilis voluptate vel quas autem rem Quis sunt et beatae officia. 
-            Ut sequi eligendi qui voluptas molestias id voluptatibus dolore ea 
-            magnam aliquam."
-
-            img="src/assets/dog.jpg"
-          />
-
-          <CardView
-            nameOfEvent="Jornada do Conhecimento"
-            initialDate="22/02/2024"
-            finishDate="26/02/2024"
-            initialTime="10:00"
-            finishTime="14:00"
-            localEvent="Ca300"
-            responsible_area="ETS - DS"
-
-            description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
-            quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
-            pariatur dolore in quae eveniet? Et dolore asperiores aut nostrum 
-            necessitatibus ex error modi ut veniam dolore 33 omnis error sed 
-            molestias velit qui blanditiis odio. Ut galisum animi et assumenda 
-            assumenda aut itaque maxime ut soluta incidunt non labore totam qui 
-            doloribus soluta ut suscipit galisum. Et aliquam rerum eum accusantium 
-            dignissimos et quia minima. Est recusandae placeat At officiis ipsa ut 
-            exercitationem ratione ea exercitationem eaque eos natus enim.
-
-            Est incidunt dignissimos ut veritatis blanditiis sit nesciunt earum et 
-            nihil commodi id repellendus molestias. Et voluptatibus laborum ex labore 
-            cupiditate et minus voluptas. Aut labore recusandae et commodi blanditiis 
-            et dolorum sint sed voluptatem quasi. Et sequi libero aut quibusdam autem 
-            ut facilis voluptate vel quas autem rem Quis sunt et beatae officia. 
-            Ut sequi eligendi qui voluptas molestias id voluptatibus dolore ea 
-            magnam aliquam."
-
-            img="src/assets/dog.jpg"
-          />
-
-          <CardView
-            nameOfEvent="Jornada do Conhecimento"
-            initialDate="22/02/2024"
-            finishDate="26/02/2024"
-            initialTime="10:00"
-            finishTime="14:00"
-            localEvent="Ca300"
-            responsible_area="HR - GE"
-
-            description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
-            quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
-            pariatur dolore in quae eveniet? Et dolore asperiores aut nostrum 
-            necessitatibus ex error modi ut veniam dolore 33 omnis error sed 
-            molestias velit qui blanditiis odio. Ut galisum animi et assumenda 
-            assumenda aut itaque maxime ut soluta incidunt non labore totam qui 
-            doloribus soluta ut suscipit galisum. Et aliquam rerum eum accusantium 
-            dignissimos et quia minima. Est recusandae placeat At officiis ipsa ut 
-            exercitationem ratione ea exercitationem eaque eos natus enim.
-
-            Est incidunt dignissimos ut veritatis blanditiis sit nesciunt earum et 
-            nihil commodi id repellendus molestias. Et voluptatibus laborum ex labore 
-            cupiditate et minus voluptas. Aut labore recusandae et commodi blanditiis 
-            et dolorum sint sed voluptatem quasi. Et sequi libero aut quibusdam autem 
-            ut facilis voluptate vel quas autem rem Quis sunt et beatae officia. 
-            Ut sequi eligendi qui voluptas molestias id voluptatibus dolore ea 
-            magnam aliquam."
-
-            img="src/assets/dog.jpg"
-          />
-
-          <CardView
-            nameOfEvent="Jornada do Conhecimento"
-            initialDate="22/02/2024"
-            finishDate="26/02/2024"
-            initialTime="10:00"
-            finishTime="14:00"
-            localEvent="Ca300"
-            responsible_area="BD - UX/UI"
-
-            description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
-            quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
-            pariatur dolore in quae eveniet? Et dolore asperiores aut nostrum 
-            necessitatibus ex error modi ut veniam dolore 33 omnis error sed 
-            molestias velit qui blanditiis odio. Ut galisum animi et assumenda 
-            assumenda aut itaque maxime ut soluta incidunt non labore totam qui 
-            doloribus soluta ut suscipit galisum. Et aliquam rerum eum accusantium 
-            dignissimos et quia minima. Est recusandae placeat At officiis ipsa ut 
-            exercitationem ratione ea exercitationem eaque eos natus enim.
-
-            Est incidunt dignissimos ut veritatis blanditiis sit nesciunt earum et 
-            nihil commodi id repellendus molestias. Et voluptatibus laborum ex labore 
-            cupiditate et minus voluptas. Aut labore recusandae et commodi blanditiis 
-            et dolorum sint sed voluptatem quasi. Et sequi libero aut quibusdam autem 
-            ut facilis voluptate vel quas autem rem Quis sunt et beatae officia. 
-            Ut sequi eligendi qui voluptas molestias id voluptatibus dolore ea 
-            magnam aliquam."
-
-            img="src/assets/dog.jpg"
-          />
-
-          <CardView
-            nameOfEvent="Jornada do Conhecimento"
-            initialDate="22/02/2024"
-            finishDate="26/02/2024"
-            initialTime="10:00"
-            finishTime="14:00"
-            localEvent="Ca300"
-            responsible_area="AMB - DS"
-
-            description="Lorem ipsum dolor sit amet. Et asperiores numquam sit totam 
-            quod sit galisum deserunt ut consequatur modi aut molestias incidunt in 
-            pariatur dolore in quae eveniet? Et dolore asperiores aut nostrum 
-            necessitatibus ex error modi ut veniam dolore 33 omnis error sed 
-            molestias velit qui blanditiis odio. Ut galisum animi et assumenda 
-            assumenda aut itaque maxime ut soluta incidunt non labore totam qui 
-            doloribus soluta ut suscipit galisum. Et aliquam rerum eum accusantium 
-            dignissimos et quia minima. Est recusandae placeat At officiis ipsa ut 
-            exercitationem ratione ea exercitationem eaque eos natus enim.
-
-            Est incidunt dignissimos ut veritatis blanditiis sit nesciunt earum et 
-            nihil commodi id repellendus molestias. Et voluptatibus laborum ex labore 
-            cupiditate et minus voluptas. Aut labore recusandae et commodi blanditiis 
-            et dolorum sint sed voluptatem quasi. Et sequi libero aut quibusdam autem 
-            ut facilis voluptate vel quas autem rem Quis sunt et beatae officia. 
-            Ut sequi eligendi qui voluptas molestias id voluptatibus dolore ea 
-            magnam aliquam."
-
-            img="src/assets/dog.jpg"
-          />
-
-        </div>
+        <div className="all-events-content">{eventCards}</div>
       </div>
     </div>
   );
